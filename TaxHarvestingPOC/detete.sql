917fe366fb70bfdb92b40e5d3d5d1fe182742bb4
amandeep2github <amnadeep.cs@gmail.com>
2017-09-05 18:33:46 +0530

    delete sql

--- TaxHarvestingPOC/detete.sql
+++ TaxHarvestingPOC/detete.sql
@@ -1,4 +1,7 @@
+delete from hybo.tlhrunportfoliohistory_tlhrunallocationhistory;
+delete from hybo.tlhrunallocationhistory;
+delete from hybo.tlhrunportfoliohistory;
 delete from hybo.tlhadvice_recommendation;
 delete from hybo.recommendation;
 delete from hybo.tlhadvice;
-delete from hybo.portfoliotaxalphahistory; 
+delete from hybo.portfoliotaxalphahistory;
